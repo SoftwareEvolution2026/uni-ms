7f894d34e88a77d609fad3369f003bc021559ae7
austin wamono <austinwamono98@gmail.com>
2026-07-18 01:31:53 +0300

    created the student controller class

---
+++ backend/src/main/java/com/uni/ms/student/controller/StudentController.java
@@ -0,0 +1,14 @@
+package com.uni.ms.student.controller;
+
+import com.uni.ms.student.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/students")
+@RequiredArgsConstructor
+public class StudentController {
+
+    private final StudentService studentService;
+}
