18c653ec6a5f0da64bfb4cdacd94ac3c21252d2c
austin wamono <austinwamono98@gmail.com>
2026-07-18 01:27:54 +0300

    added the student service class file

---
+++ backend/src/main/java/com/uni/ms/student/service/StudentService.java
@@ -0,0 +1,12 @@
+package com.uni.ms.student.service;
+
+import com.uni.ms.student.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+}
